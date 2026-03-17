bcab5951ee8db1707e7d7b9ceadda6b6c6aa2b75
JojoXsnipeR <joris_payen94@icloud.com>
2026-03-17 11:27:20 +0100

    Update main.tsx
    ctrl z

--- src/main.tsx
+++ src/main.tsx
@@ -914,6 +914,7 @@ const Main: React.FC = () => {
                                                     <polyline points="12 16 16 12 12 8"></polyline>
                                                     <line x1="8" y1="12" x2="16" y2="12"></line>
                                                 </svg>
+                                                Voir les Details
                                             </a>
                                         </div>
                                     </div>
